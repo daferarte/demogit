852b49b54052c6e023b96a6829c0a39b19831428
daferarte <daferarte@gmail.com>
2025-06-06 20:18:58 -0500

    agregando metodo para saber nombre

--- persona.py
+++ persona.py
@@ -2,3 +2,6 @@ class persona:
     def __init__(self, nombre, apellido):
         self.nombre = nombre
         self.apellido = apellido
+        
+    def DarNombre(self):
+        return self.nombre
