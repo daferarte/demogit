b1008612022f3d4438bc5bd9c54dacf9f732f228
daferarte <daferarte@gmail.com>
2025-06-06 20:00:15 -0500

    inicio de mi proyecto

---
+++ carro.py
@@ -0,0 +1,8 @@
+class Carro:
+    def __init__(self, marca):
+        self.color='rojo'
+        self.marca=marca
+        self.velocidad=10
+        
+    def frenar(self):
+        self.velocidad=0
